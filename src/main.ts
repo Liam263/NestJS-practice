11bcbe42b896c0ecb35db593f469c25375070653
Liam263 <nguyenthanhlam091@gmail.com>
2024-08-29 10:41:14 +1000

    Revert "change port to 4000"
    This reverts commit d5d3c53824b98d2594c83e9be3e7b8d58c814428.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  await app.listen(4000);
+  await app.listen(3000);
 }
 bootstrap();
