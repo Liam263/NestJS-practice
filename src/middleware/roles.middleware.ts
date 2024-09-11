ad424e02bf6f7caf3a8875e4b0a007bf09aa4ec2
Liam263 <nguyenthanhlam091@gmail.com>
2024-09-11 12:41:33 +1000

    add module and middleware

---
+++ src/middleware/roles.middleware.ts
@@ -0,0 +1,23 @@
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class RolesMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const user = req.headers['role'];
+    const route = req.originalUrl;
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    console.log(user);
+    if (user !== 'admin' && route.startsWith('/users/admin')) {
+      return res.status(403).json({ message: 'Access forbidden: Admin only' });
+    }
+
+    next();
+  }
+}
