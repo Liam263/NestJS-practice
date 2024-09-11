823ee81b83edb64f5c50456598852d033b303b3f
Liam263 <nguyenthanhlam091@gmail.com>
2024-09-11 12:38:13 +1000

    add user module

---
+++ src/modules/user.module.ts
@@ -0,0 +1,17 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from 'src/controllers/user.controller';
+import { RolesMiddleware } from 'src/middleware/roles.middleware';
+import { UserSchema } from 'src/repositories/schemas/user.schema';
+import { UserService } from 'src/services/user.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  controllers: [UsersController],
+  providers: [UserService],
+})
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RolesMiddleware).forRoutes(UsersController);
+  }
+}
